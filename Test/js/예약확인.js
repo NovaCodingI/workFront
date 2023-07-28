8c9d375d9bbf5d7d53d6dad20f89757d52ee7dca
ikikjun <ikikjun@naver.com>
2023-07-28 18:01:52 +0900

    20230728

---
+++ Test/js/예약확인.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#confirmButton').click(function() {
+      window.location.href = 'my_page.html';
+    });
+    
+    $('#backButton').click(function() {
+      window.location.href = 'instructor_list.html';
+    });
+  });
